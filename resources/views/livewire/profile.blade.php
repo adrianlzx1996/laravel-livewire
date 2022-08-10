8e039d5efd5a4d1a1e7abd5297bf656a813015d5
Adrian Leong <leongadrian36@gmail.com>
2022-08-10 20:31:59 +0800

    show temporary uploaded image

--- resources/views/livewire/profile.blade.php
+++ resources/views/livewire/profile.blade.php
@@ -29,9 +29,12 @@
 					<x-input.group label="Photo" for="newAvatar" :error="$errors->first('newAvatar')">
 						<div class="flex items-center">
 							<span class="h-12 w-12 rounded-full overflow-hidden bg-gray-100">
-								<img src="{{ auth()->user()->avatarUrl() }}" alt="Profile Photo">
+								@if($newAvatar)
+									<img src="{{ $newAvatar->temporaryUrl() }}" alt="Profile Photo">
+								@else
+									<img src="{{ auth()->user()->avatarUrl() }}" alt="Profile Photo">
+								@endif
 							</span>
-
 
 							<span class="ml-5 rounded-md shadow-sm">
 								<input type="file" wire:model="newAvatar">
