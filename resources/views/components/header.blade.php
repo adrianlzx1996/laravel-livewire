7fd288fb4faeb5da413fd5e59b966fe81daa3c0e
Adrian Leong <leongadrian36@gmail.com>
2022-08-10 12:51:53 +0800

    header

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,15 @@
+<!-- Navbar -->
+<nav class="bg-gray-50">
+	<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+		<div class="relative h-16 flex items-center justify-between border-b border-gray-200">
+			<div class="flex-shrink-0">
+				<x-icons.app/>
+			</div>
+
+			<x-nav.nav/>
+
+		</div>
+	</div>
+
+	<x-nav.mobile/>
+</nav>
