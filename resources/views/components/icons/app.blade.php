6222589964c0209c173086bcac39222b746d5a75
Adrian Leong <leongadrian36@gmail.com>
2022-08-10 12:51:27 +0800

    app logo

---
+++ resources/views/components/icons/app.blade.php
@@ -0,0 +1,8 @@
+@props([
+	'class' => 'h-8 w-auto',
+])
+<img
+	class="{{ $class }}"
+	{{ $attributes }}
+	src="https://tailwindui.com/img/logos/workflow-mark.svg?color=violet&shade=500"
+	alt="Workflow">
