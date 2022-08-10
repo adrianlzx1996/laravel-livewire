da8923bd3b52e86e6630c669740d5adce86e9c8a
Adrian Leong <leongadrian36@gmail.com>
2022-08-10 18:39:00 +0800

    avatar url

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@
 	use Illuminate\Database\Eloquent\Factories\HasFactory;
 	use Illuminate\Foundation\Auth\User as Authenticatable;
 	use Illuminate\Notifications\Notifiable;
+	use Illuminate\Support\Facades\Storage;
 	use Laravel\Sanctum\HasApiTokens;
 
 	class User extends Authenticatable
@@ -51,6 +52,8 @@
 
 		public function avatarUrl ()
 		{
-			return "https://www.gravatar.com/avatar/" . md5(strtolower(trim($this->email)));
+			return $this->avatar
+				? Storage::disk('avatars')->url($this->avatar)
+				: "https://www.gravatar.com/avatar/" . md5(strtolower(trim($this->email)));
 		}
 	}
