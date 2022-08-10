d113936886c634f3c6e0e356616b719c1a72f09a
Adrian Leong <leongadrian36@gmail.com>
2022-08-10 12:51:42 +0800

    nav component

---
+++ resources/views/components/nav/mobile.blade.php
@@ -0,0 +1,50 @@
+<!-- Mobile menu, show/hide based on menu state. -->
+<div class="bg-gray-50 border-b border-gray-200 lg:hidden" id="mobile-menu">
+	<div class="px-2 pt-2 pb-3 space-y-1">
+		<!-- Current: "bg-gray-100", Default: "hover:bg-gray-100" -->
+		<a href="#" class="bg-gray-100 block px-3 py-2 rounded-md font-medium text-gray-900"
+		   aria-current="page">Dashboard</a>
+
+		<a href="#" class="hover:bg-gray-100 block px-3 py-2 rounded-md font-medium text-gray-900">Jobs</a>
+
+		<a href="#"
+		   class="hover:bg-gray-100 block px-3 py-2 rounded-md font-medium text-gray-900">Applicants</a>
+
+		<a href="#" class="hover:bg-gray-100 block px-3 py-2 rounded-md font-medium text-gray-900">Company</a>
+	</div>
+	<div class="pt-4 pb-3 border-t border-gray-200">
+		<div class="px-5 flex items-center">
+			<div class="flex-shrink-0">
+				<img class="rounded-full h-10 w-10"
+				     src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+				     alt="">
+			</div>
+			<div class="ml-3">
+				<div class="text-base font-medium text-gray-800">Whitney Francis</div>
+				<div class="text-sm font-medium text-gray-500">whitney.francis@example.com</div>
+			</div>
+			<button type="button"
+			        class="ml-auto bg-gray-50 flex-shrink-0 rounded-full p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-purple-500">
+				<span class="sr-only">View notifications</span>
+				<!-- Heroicon name: outline/bell -->
+				<svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+				     stroke="currentColor" aria-hidden="true">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+					      d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"/>
+				</svg>
+			</button>
+		</div>
+		<div class="mt-3 px-2 space-y-1">
+			<a href="#"
+			   class="block rounded-md py-2 px-3 text-base font-medium text-gray-900 hover:bg-gray-100">Your
+				Profile</a>
+
+			<a href="#"
+			   class="block rounded-md py-2 px-3 text-base font-medium text-gray-900 hover:bg-gray-100">Settings</a>
+
+			<a href="#"
+			   class="block rounded-md py-2 px-3 text-base font-medium text-gray-900 hover:bg-gray-100">Sign
+				out</a>
+		</div>
+	</div>
+</div>
