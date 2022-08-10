5e826397b596a839874f68cedd5fcba9300d0ee3
Adrian Leong <leongadrian36@gmail.com>
2022-08-10 12:52:16 +0800

    dashboard

---
+++ resources/views/livewire/dashboard.blade.php
@@ -0,0 +1,11 @@
+<div class="min-h-full">
+	<x-header/>
+	{{--	<x-page.breadcrumb/>--}}
+
+	<x-main>
+		<!-- This example requires Tailwind CSS v2.0+ -->
+		<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+			<!-- Content goes here -->
+		</div>
+	</x-main>
+</div>
