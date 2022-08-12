88f9d5ac163d94dc3f64f103a4627341cca80c13
Adrian Leong <leongadrian36@gmail.com>
2022-08-12 21:47:33 +0800

    search function

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -10,10 +10,12 @@
 	{
 		use WithPagination;
 
+		public $search = '';
+
 		public function render ()
 		{
 			return view('livewire.dashboard', [
-				'transactions' => Transaction::paginate(10),
+				'transactions' => Transaction::search('title', $this->search)->paginate(10),
 			]);
 		}
 	}
