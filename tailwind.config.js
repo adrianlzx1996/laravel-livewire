1c1aba726a1ebc242bef3e59453ed49c19a2ca34
Adrian Leong <leongadrian36@gmail.com>
2022-08-12 21:05:05 +0800

    safelist for dynamic classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
 		"./resources/**/*.blade.php",
 		"./resources/**/*.js",
 	],
+	safelist: [
+		{
+			pattern: /text-(red|green|blue|slate)-(500|600|700|800|900)/,
+		},
+		{
+			pattern: /bg-(red|green|blue|slate)-(100|200|300)/,
+		},
+	],
 	theme: {
 		extend: {},
 	},
