5ad6c15397dfc1f0e2c7d159eb0c782b591e06f8
Adrian Leong <leongadrian36@gmail.com>
2022-08-12 21:24:18 +0800

    pagination

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -2,46 +2,52 @@
 	<h1 class="text-2xl font-semibold text-gray-900">Dashboard</h1>
 
 	<div class="py-4">
-		<x-table>
-			<x-slot name="head">
-				<x-table.heading sortable>Title</x-table.heading>
-				<x-table.heading sortable>Amount</x-table.heading>
-				<x-table.heading sortable>Status</x-table.heading>
-				<x-table.heading sortable>Date</x-table.heading>
-			</x-slot>
+		<div class="flex-col space-y-4">
+			<x-table>
+				<x-slot name="head">
+					<x-table.heading sortable>Title</x-table.heading>
+					<x-table.heading sortable>Amount</x-table.heading>
+					<x-table.heading sortable>Status</x-table.heading>
+					<x-table.heading sortable>Date</x-table.heading>
+				</x-slot>
 
-			<x-slot name="body">
-				@foreach($transactions as $transaction)
-					<x-table.row>
-						<x-table.cell>
+				<x-slot name="body">
+					@foreach($transactions as $transaction)
+						<x-table.row>
+							<x-table.cell>
 							<span class="inline-flex space-x-2 truncate text-sm">
 								<x-icons.cash/>
 								<p class="text-cool-gray-600 truncate">{{ $transaction->title }}</p>
 							</span>
-						</x-table.cell>
+							</x-table.cell>
 
-						<x-table.cell class="text-slate-500">
+							<x-table.cell class="text-slate-500">
 							<span class="text-slate-900 font-medium">
 								${{ $transaction->amount }}
 							</span>
-							USD
-						</x-table.cell>
+								USD
+							</x-table.cell>
 
-						<x-table.cell>
+							<x-table.cell>
 							<span
 								class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-{{ $transaction->status_color }}-100 text-{{ $transaction->status_color }}-800 capitalize">
 								{{ $transaction->status }}
 							</span>
-						</x-table.cell>
+							</x-table.cell>
 
-						<x-table.cell>
-							<time datetime="{{ $transaction->date }}">
-								{{ $transaction->date_for_humans }}
-							</time>
-						</x-table.cell>
-					</x-table.row>
-				@endforeach
-			</x-slot>
-		</x-table>
+							<x-table.cell>
+								<time datetime="{{ $transaction->date }}">
+									{{ $transaction->date_for_humans }}
+								</time>
+							</x-table.cell>
+						</x-table.row>
+					@endforeach
+				</x-slot>
+			</x-table>
+
+			<div>
+				{{ $transactions->links() }}
+			</div>
+		</div>
 	</div>
 </div>
