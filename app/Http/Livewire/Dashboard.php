33ffe2fae1d9a1d983a371d002603394aa2ba48a
Adrian Leong <leongadrian36@gmail.com>
2022-08-12 22:01:41 +0800

    sorting

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -3,6 +3,9 @@
 	namespace App\Http\Livewire;
 
 	use App\Models\Transaction;
+	use Illuminate\Contracts\Foundation\Application;
+	use Illuminate\Contracts\View\Factory;
+	use Illuminate\Contracts\View\View;
 	use Livewire\Component;
 	use Livewire\WithPagination;
 
@@ -10,12 +13,26 @@
 	{
 		use WithPagination;
 
-		public $search = '';
+		public $search        = '';
+		public $sortField     = 'title';
+		public $sortDirection = 'asc';
+
+		public function sortBy ( $field )
+		: void {
+			if ( $this->sortField === $field ) {
+				$this->sortDirection = $this->sortDirection === 'asc' ? 'desc' : 'asc';
+			}
+			else {
+				$this->sortField     = $field;
+				$this->sortDirection = 'asc';
+			}
+		}
 
 		public function render ()
+		: Factory|View|Application
 		{
 			return view('livewire.dashboard', [
-				'transactions' => Transaction::search('title', $this->search)->paginate(10),
+				'transactions' => Transaction::search('title', $this->search)->orderBy($this->sortField, $this->sortDirection)->paginate(10),
 			]);
 		}
 	}
