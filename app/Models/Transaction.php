c07d24e101bf0ffa6849d411f433a563decc3c8e
Adrian Leong <leongadrian36@gmail.com>
2022-08-12 21:05:25 +0800

    status color

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -1,11 +1,22 @@
 <?php
 
-namespace App\Models;
+	namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+	use Illuminate\Database\Eloquent\Factories\HasFactory;
+	use Illuminate\Database\Eloquent\Model;
 
-class Transaction extends Model
-{
-    use HasFactory;
-}
+	class Transaction extends Model
+	{
+		use HasFactory;
+
+		public $casts = [ 'date' => 'date' ];
+
+		public function getStatusColorAttribute ()
+		{
+			return [
+					   'new'     => 'blue',
+					   'success' => 'green',
+					   'failed'  => 'red',
+				   ][$this->status] ?? 'slate';
+		}
+	}
