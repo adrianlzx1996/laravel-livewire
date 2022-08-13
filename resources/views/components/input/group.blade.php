2756670a68395772fb414ed9ee381c90aa890838
Adrian Leong <leongadrian36@gmail.com>
2022-08-13 11:01:58 +0800

    style

--- resources/views/components/input/group.blade.php
+++ resources/views/components/input/group.blade.php
@@ -2,10 +2,12 @@
 	'label',
 	'for',
 	'error' => false,
+	'inline' => false,
 	'helpText' => '',
 ])
 
-<div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+<div
+	class="sm:items-start sm:border-t sm:border-gray-200 sm:pt-5 {{ $inline ? 'inline-flex' : 'sm:grid sm:grid-cols-3 sm:gap-4' }}">
 	<label for="{{ $for }}" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
 		{{ $label }} </label>
 	<div class="mt-1 sm:mt-0 sm:col-span-2">
