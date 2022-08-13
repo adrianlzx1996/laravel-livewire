7c6f07ae91e26feef3d43b6e879129b64ccb0f36
Adrian Leong <leongadrian36@gmail.com>
2022-08-13 09:38:52 +0800

    style

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -6,7 +6,7 @@
 @php $color = $color === 'default' ? 'text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500' : $color @endphp
 <button
 	type="{{ $type }}"
-	{{ $attributes->class(["w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:outline-none $color transition duration-300"]) }}
+	{{ $attributes->class(["flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:outline-none $color transition duration-300"]) }}
 >
 	{{ $slot }}
 </button>
