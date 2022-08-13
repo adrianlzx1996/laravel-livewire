4856d3386e73a6ef447fbaebc53bbfde892ebd97
Adrian Leong <leongadrian36@gmail.com>
2022-08-13 13:34:08 +0800

    multi column sorting

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -75,18 +75,18 @@
 					<x-table.heading class="pr-0 w-8">
 						<input type="checkbox" wire:model="selectPage"/>
 					</x-table.heading>
-					<x-table.heading sortable wire:click="sortBy('title')"
+					<x-table.heading sortable multi-column wire:click="sortBy('title')"
 					                 class="w-full"
-					                 :direction="$sortField === 'title' ? $sortDirection : null">Title
+					                 :direction="$sorts['title'] ?? null">Title
 					</x-table.heading>
-					<x-table.heading sortable wire:click="sortBy('amount')"
-					                 :direction="$sortField === 'amount' ? $sortDirection : null">Amount
+					<x-table.heading sortable multi-column wire:click="sortBy('amount')"
+					                 :direction="$sorts['amount'] ?? null">Amount
 					</x-table.heading>
-					<x-table.heading sortable wire:click="sortBy('status')"
-					                 :direction="$sortField === 'status' ? $sortDirection : null">Status
+					<x-table.heading sortable multi-column wire:click="sortBy('status')"
+					                 :direction="$sorts['status'] ?? null">Status
 					</x-table.heading>
-					<x-table.heading sortable wire:click="sortBy('date')"
-					                 :direction="$sortField === 'date' ? $sortDirection : null">Date
+					<x-table.heading sortable multi-column wire:click="sortBy('date')"
+					                 :direction="$sorts['date'] ?? null">Date
 					</x-table.heading>
 					<x-table.heading/>
 				</x-slot>
