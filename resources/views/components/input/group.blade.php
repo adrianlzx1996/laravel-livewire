7f34d806da4e5f75b6b17d64c3677f01743a6f91
Adrian Leong <leongadrian36@gmail.com>
2022-08-14 11:21:05 +0800

    style

--- resources/views/components/input/group.blade.php
+++ resources/views/components/input/group.blade.php
@@ -4,10 +4,12 @@
 	'error' => false,
 	'inline' => false,
 	'helpText' => '',
+	'borderless' => false,
+	'paddingless' => false,
 ])
 
 <div
-	class="sm:items-start sm:border-t sm:border-gray-200 sm:pt-5 {{ $inline ? 'inline-flex' : 'sm:grid sm:grid-cols-3 sm:gap-4' }}">
+	class="sm:items-start {{ $borderless ? '' : 'sm:border-t sm:border-gray-200' }} {{ $paddingless ? '' : 'sm:pt-5' }} {{ $inline ? 'inline-flex' : 'sm:grid sm:grid-cols-3 sm:gap-4' }}">
 	<label for="{{ $for }}" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
 		{{ $label }} </label>
 	<div class="mt-1 sm:mt-0 sm:col-span-2">
